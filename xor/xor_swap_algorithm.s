69176aebc77d45ba2d4d35046ab1bb67aa190379
われ <74850252+ware255@users.noreply.github.com>
2022-11-04 20:07:35 +0900

    xor_swap
    忘備録：jnzは、「0ではなかったとき○○する。」という命令

--- xor/xor_swap_algorithm.s
+++ xor/xor_swap_algorithm.s
@@ -1,11 +1,19 @@
 .globl main
 
-main:
-    mov    $1, %rax
-    mov    $5, %rbx
-    
+xor_swap:
+    mov    %rcx, %rax
+
     xor    %rax, %rbx
     xor    %rbx, %rax
     xor    %rax, %rbx
-    
+
+    ret
+
+main:
+    mov    $1, %rax
+    mov    $5, %rbx
+
+    mov    %rax, %rcx
+    sub    %rbx, %rax
+    jnz    xor_swap
     ret
