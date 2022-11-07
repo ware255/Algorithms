8ad720beb4ff04c39fa135f744dc44a46f415ee8
われ <74850252+ware255@users.noreply.github.com>
2022-11-07 22:09:52 +0900

    Create GCD.s

---
+++ GCD.s
@@ -0,0 +1,20 @@
+.globl main
+
+gcd:
+    xor    %rdx, %rdx
+    div    %rbx
+    mov    %rdx, %rbx
+    cmp    $0, %rdx
+    jz     gcd_end
+    jmp    gcd
+    ret
+
+gcd_end:
+    ret
+
+main:
+    mov    $20, %rax
+    mov    $3, %rbx
+    call   gcd
+    ret
+
